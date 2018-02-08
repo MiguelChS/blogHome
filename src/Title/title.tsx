d65f7f9c9c90d22087c294bfc577fd918b5e6b41
miguel.chauca@almundo.com <miguel.chauca@almundo.com>
2018-02-08 18:57:03 -0300

    configuracion de proyecto

---
+++ src/Title/title.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import {title} from './title.css' 
+
+export default ()=>{
+    return(
+        <h1 className={title}>
+            Este El titulo
+        </h1>
+    )
+}
